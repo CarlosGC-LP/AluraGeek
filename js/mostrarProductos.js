4e4e9bb3be699a5b7ff02b6d99ace1b634fac9a3
Carlos Gonzalez <carlos.custodi@hotmail.com>
2025-01-09 21:33:13 -0500

    Update mostrarProductos.js

--- js/mostrarProductos.js
+++ js/mostrarProductos.js
@@ -1,13 +1,15 @@
+import { conexionAPI } from "./conexionAPI.js";
+
 const card = document.querySelector("[data-card]");
 
-function crearCard() {
+function crearCard(nombre, precio, imagen) {
     const card = document.createElement("div");
 
     card.className = "card__contenido";
     card.innerHTML = `
-        <img src="image/StarWars.png" alt="Imagen producto">
-        <p class="card__nombre">Stormtrooper</p>
-        <p class="card__precio">S/60.00</p>
+        <img src="image/${imagen}.png" alt="Imagen producto">
+        <p class="card__nombre">${nombre}r</p>
+        <p class="card__precio">S/${precio}</p>
         <button class="card__boton">
             <img src="image/iconTacho.png" alt="icono tacho">
         </button>  
@@ -18,4 +20,8 @@ function crearCard() {
 
 async function listarCard() {
     const listaAPI = await conexionAPI.listaProductos();
+
+    listaAPI.forEach(producto => lista.appendChild(crearCard(card.nombre, card.precio, card.imagen)));
 }
+
+listarCard();

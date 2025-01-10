b4e65b205321f4f728c4c35a14285397e0e867e7
Carlos Gonzalez <carlos.custodi@hotmail.com>
2025-01-10 18:16:41 -0500

    Update crearProducto.js

--- js/crearProducto.js
+++ js/crearProducto.js
@@ -1,7 +1,15 @@
+import { conexionAPI } from "./conexionAPI.js";
+
 const formulario = document.querySelector("data-formulario");
 
-function crearProducto() {
+async function crearProducto(evento) {
+    evento.preventDefault();
+
     const nombre = document.querySelector("[data-nombre]").value;
     const precio = document.querySelector("[data-precio]").value;
     const imagen = document.querySelector("[data-imagen]").value;
+
+    await conexionAPI.crearCard(nombre, precio, imagen);
 }
+
+formulario.addEventListener("submit", evento => crearProducto(evento));

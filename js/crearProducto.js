5df9a0c19c4687a187e347e10b7d6f05c742b5b9
Carlos Gonzalez <carlos.custodi@hotmail.com>
2025-01-10 16:57:07 -0500

    Update crearProducto.js

--- js/crearProducto.js
+++ js/crearProducto.js
@@ -1,5 +1,7 @@
 const formulario = document.querySelector("data-formulario");
 
 function crearProducto() {
-    
+    const nombre = document.querySelector("[data-nombre]").value;
+    const precio = document.querySelector("[data-precio]").value;
+    const imagen = document.querySelector("[data-imagen]").value;
 }

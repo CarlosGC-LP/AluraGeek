160bcc741981639815aac8a4f6b1d6a7d846af44
Carlos Gonzalez <carlos.custodi@hotmail.com>
2025-01-10 12:46:29 -0500

    Update conexionAPI.js

--- js/conexionAPI.js
+++ js/conexionAPI.js
@@ -6,8 +6,26 @@ async function listaProductos() {
     return conexionConvertida;
 }
 
+async function crearCard(nombre, precio, imagen) {
+    const conexion = await fetch("http://localhost:3001/productos", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            nombre: nombre,
+            precio: precio,
+            imagen: imagen
+        })
+    });
+
+    const conexionConvertida = conexion.json();
+
+    return conexionConvertida;
+}
+
 export const conexionAPI = {
-    listaProductos
+    listaProductos, crearCard
 }
 
 //listaProductos();

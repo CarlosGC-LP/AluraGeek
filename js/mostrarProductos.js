ebbd47c1556cbe89f63dfde835fea49881c289a5
Carlos Gonzalez <carlos.custodi@hotmail.com>
2025-01-10 22:11:44 -0500

    Update mostrarProductos.js

--- js/mostrarProductos.js
+++ js/mostrarProductos.js
@@ -2,7 +2,7 @@ import { conexionAPI } from "./conexionAPI.js";
 
 const card = document.querySelector("[data-card]");
 
-function crearCard(nombre, precio, imagen) {
+function crearCard(id, nombre, precio, imagen) {
     const producto = document.createElement("div");
 
     producto.className = "card__contenido";
@@ -10,10 +10,13 @@ function crearCard(nombre, precio, imagen) {
         <img class="card__imagen" src="${imagen}" alt="Imagen producto">
         <p class="card__nombre">${nombre}</p>
         <p class="card__precio">S/${precio}</p>
-        <button class="card__boton">
+        <button class="card__boton" data-eliminar="${id}">
             <img src="image/iconTacho.png" alt="icono tacho">
         </button>  
     `;
+
+    const botonEliminar = producto.querySelector("[data-eliminar]");
+    botonEliminar.addEventListener("click", () => eliminarProducto(id, producto));
 
     return producto;
 }
@@ -21,7 +24,17 @@ function crearCard(nombre, precio, imagen) {
 async function listarCard() {
     const listaAPI = await conexionAPI.listaProductos();
 
-    listaAPI.forEach(producto => card.appendChild(crearCard(producto.nombre, producto.precio, producto.imagen)));
+    listaAPI.forEach(producto => card.appendChild(crearCard(producto.id, producto.nombre, producto.precio, producto.imagen)));
+}
+
+async function eliminarProducto(id, producto) {
+    const respuesta = await conexionAPI.eliminarProducto(id);
+    
+    if (respuesta) {
+        producto.remove();
+    } else {
+        console.error("Hubo un error al eliminar el producto");
+    }
 }
 
 listarCard();
